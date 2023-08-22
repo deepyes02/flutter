d926badcbd23baecd9860b3ae5cd2d6d4f07d47f
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2023-08-22 11:52:01 +0900

    Hello world+

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: Page1()));
-}
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(_createRoute());
-                },
-                child: const Text('Go!'))));
-  }
-}
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-        child,
+void main(){
+  runApp(
+    const Text('Hello World', textDirection: TextDirection.ltr)
   );
-}
-
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(), body: const Center(child: Text('Page 2')));
-  }
 }
